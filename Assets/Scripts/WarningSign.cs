8a31bd63c3698d1e9e59c75a0833a3c4b3fa129d
jaspercmurphy <jaspercmurphy@gmail.com>
2021-01-08 17:50:34 -0600

    WarningSignForObstacles

---
+++ Assets/Scripts/WarningSign.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WarningSign : MonoBehaviour
+{
+    
+    public float blinkRate = 0.2f;
+    public float blinkTimer = 0.0f;
+    public float lifespan = Spawner.warningDelay;
+    public bool isVisible;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector3(9.0f, transform.position.y, transform.position.z);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (blinkTimer > blinkRate){
+            gameObject.GetComponent<SpriteRenderer>().enabled = isVisible;
+            isVisible = !isVisible;
+            blinkTimer = 0;
+        }
+        
+        if (lifespan < 0.0f) {
+            Destroy(gameObject);
+        }
+        
+        lifespan -= Time.deltaTime;
+        blinkTimer += Time.deltaTime;
+    }
+}
