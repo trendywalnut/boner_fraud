ed828a3aa3646004ab1b4d5edac121d8d53958a3
jojohorak <jehorak@utexas.edu>
2021-01-08 16:46:51 -0600

    dialoge System

---
+++ Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueManager : MonoBehaviour
+{
+
+    public GameObject[] dialogueBoxes;
+
+    public int dialogueI;
+
+    private void Start()
+    {
+        dialogueI = 0;
+        dialogueBoxes[0].SetActive(true);
+
+        for (int i = 1; i < dialogueBoxes.Length; i++)
+        {
+            dialogueBoxes[i].SetActive(false);
+        }
+    }
+
+    private void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Clicked");
+            Debug.Log(dialogueI);
+            dialogueBoxes[dialogueI].SetActive(false);
+            dialogueBoxes[dialogueI + 1].SetActive(true);
+            dialogueI++;
+        }
+    }
+}
