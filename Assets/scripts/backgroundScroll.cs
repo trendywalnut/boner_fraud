72bdf32c91b9d7103ee3c71911bb59538586f34d
trendywalnut <connorb23@utexas.edu>
2021-01-08 15:30:35 -0600

    editing my scene and added tmp

---
+++ Assets/scripts/backgroundScroll.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class backgroundScroll : MonoBehaviour
+{
+
+    Material material;
+    Vector2 offset;
+
+    public int xVel;
+
+    private void Awake()
+    {
+        material = GetComponent<Renderer>().material;
+    }
+
+    void Start()
+    {
+        offset = new Vector2(xVel, 0);
+    }
+
+    void Update()
+    {
+        material.mainTextureOffset += offset * Time.deltaTime;
+        
+    }
+}
