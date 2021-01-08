c1bd0973eb829fe44afee2c2ec0566ccc6e114f8
jaspercmurphy <jaspercmurphy@gmail.com>
2021-01-08 15:29:41 -0600

    AddedJaperScene

---
+++ Assets/Scripts/EnemyCar.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyCar : MonoBehaviour
+{
+    
+    private float newCarSpeed;
+    public Spawner mySpawner;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        mySpawner = GameObject.FindGameObjectsWithTag("Spawner");
+        newCarSpeed = mySpawner.carSpeed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.Translate(Vector3.left * newCarSpeed * Time.deltaTime);
+    }
+}
