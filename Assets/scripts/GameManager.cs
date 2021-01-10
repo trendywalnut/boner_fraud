2f9529c3a5963439c6e9e5a6ea1943bb4b4998b3
trendywalnut <connorb23@utexas.edu>
2021-01-10 13:26:33 -0600

    main menu adjustments

--- Assets/scripts/GameManager.cs
+++ Assets/scripts/GameManager.cs
@@ -5,10 +5,10 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    //0 - crash, 
+    //0 - crash, 1 - buzz buzz, 2 - carignition, 3 - sirens, 4 - textpop
     public AudioClip[] sfxClips;
 
-    //0 - dialogue loop, 1 - ooberwave,
+    //0 - dialogue loop, 1 - ooberwave, 2 - cassette synthwave
     public AudioClip[] musicClips;
 
     private AudioSource aS;
@@ -28,6 +28,15 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (SceneManager.GetActiveScene().buildIndex == 0)
+        {
+            if (aS.clip != musicClips[2] || !aS.isPlaying)
+            {
+                aS.clip = musicClips[2];
+                aS.Play();
+            }
+        }
+
         if (SceneManager.GetActiveScene().buildIndex == 2)
         {
             if (aS.clip != musicClips[1] || !aS.isPlaying)
