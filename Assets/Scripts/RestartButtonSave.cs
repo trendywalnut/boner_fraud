b292751943584e5f2cb73cc049ae52758464a898
jojohorak <jehorak@utexas.edu>
2021-01-10 23:27:35 -0600

    Flashing and Other (?)

--- Assets/Scripts/RestartButtonSave.cs
+++ Assets/Scripts/RestartButtonSave.cs
@@ -9,13 +9,17 @@ public class RestartButtonSave : MonoBehaviour
 
     public GameManager gMScript;
 
+    private GameObject player;
+
     private void Awake()
     {
+        player = GameObject.FindGameObjectWithTag("Player");
         gMScript = FindObjectOfType<GameManager>();
     }
 
     public void RestartLevel()
     {
-        gMScript.RestartLevel();    
+        gMScript.RestartLevel();
+        player.GetComponent<BoxCollider2D>().enabled = true;
     }
 }
